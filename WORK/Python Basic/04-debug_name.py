38a0a2d33e9c4e1a6d958f08c73e59c79ebeaf77
IreneJantarat <40989@virtual.prc.ac.th>
2025-06-18 09:32:01 +0700

    Create 04-debug_name.py

---
+++ WORK/Python Basic/04-debug_name.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+print("Hello", name)
+print("You name has " , len(name) , " letters.")
+
+#แก้ไขคำที่พิมพ์ผิดและทำให้ประโยคแสดงผลสำบูรณ์ เพื่อจะสามารถแสดงผลข้อมูลได้ตามโจทย์
