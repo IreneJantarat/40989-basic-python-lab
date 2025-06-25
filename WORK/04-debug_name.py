b067c5b62bf0a5f2f20966ddec49617aca7c4cf4
IreneJantarat <40989@virtual.prc.ac.th>
2025-06-25 09:22:22 +0700

    Delete 04-debug_name.py

--- WORK/04-debug_name.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter your name: ")
-
-print("Hello", name)
-print("You name has " , len(name) , " letters.")
-
-#แก้ไขคำที่พิมพ์ผิดและทำให้ประโยคแสดงผลสำบูรณ์ เพื่อจะสามารถแสดงผลข้อมูลได้ตามโจทย์
